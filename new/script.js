cda10df1eace3acb14440323f854f445f200b68e
D_00 <leo.daloz@epita.fr>
2024-02-26 19:26:48 +0100

    finished styling, added bottom links

--- new/script.js
+++ new/script.js
@@ -3,8 +3,8 @@ let W, H;
 
 // name, description, image (true / false)
 let repos = [
-	[["hardest", "Hardest game ever. Truly.", true, [217, 141, 141], false],
-	 ["dumb-questions", "Dumb questions, website with about 99% CSS.", false, [237, 177, 38], true],
+	[["dumb-questions", "Dumb questions, website with about 99% CSS.", false, [237, 177, 38], true],
+	 ["hardest", "Hardest game ever. Truly.", true, [217, 141, 141], false],
 	 ["finesse", "A customizable Tetris finesse trainer", true, [228, 119, 241], true],
 	 ["mandelbrot", "Javascript Mandelbrot set visualizer", true, [109, 163, 71], true],
 	 ["hanoi", "Hanoi towers game", true, [193, 253, 249], true],
@@ -14,11 +14,11 @@ let repos = [
 	 ["2048", "2048 game, created in 1 hour. Maybe some bugs.", true, [187, 173, 160], false],
 	 ["maze", "Doom-like maze with randomly generated levels", true, [95, 105, 80], false],
 	 ["cpp-renderer", "C++ 3D renderer, using SDL2", true, [187, 212, 255], false],
+	 ["jstris-plus", "Jstris+ storage for soundpacks I created", true, [229, 92, 191], true],
 	 ["install-wizard", "Rudimentary tkinter installer", false, [192, 192, 192], true],
-	 ["smooth-movement", "A PyGame smooth movement, which can be used to move a sprite with friction", false, [192, 192, 192], true],
-	 ["camera-scrolling", "The camera follows the player, to make it stay in an area in the center of the screen", false, [192, 192, 192], true],
-	 ["textbox", "A simple textbox, which can be used with pygame", false, [192, 192, 192], true],
-	 ["jstris-plus", "Jstris+ storage for soundpacks I created", true, [229, 92, 191], true]]
+	 ["smooth-movement", "pygame smooth character movement with friction", false, [192, 192, 192], true],
+	 ["camera-scrolling", "Smooth camera following script", false, [192, 192, 192], true],
+	 ["textbox", "A simple pygame textbox", false, [192, 192, 192], true]]
 	];
 
 function multColor(col, m) {
@@ -28,6 +28,7 @@ function multColor(col, m) {
 function addRepos() {
 	let sections = document.getElementsByTagName("section");
 	for (let i = 0; i < 2; i++) {
+		let container = sections[i].children[1];
 		for (let j = 0; j < repos[i].length; j++) {
 			repo = repos[i][j];
 
@@ -43,7 +44,7 @@ function addRepos() {
 
 			let A, B, C, D, E;
 			if (dark) A = "#222", B = "#1a1a1a", C = "#000", D = multColor(col, 1), E = multColor(col, 0.8);
-			else A = multColor(col, 1), B = multColor(col, 0.8), C = multColor(col, 0.4), D = "#000", E = multColor(col, 0.3);
+			else A = multColor(col, 1), B = multColor(col, 0.8), C = multColor(col, 0.5), D = "#000", E = multColor(col, 0.3);
 			block.style = "--bg1: "+A+"; --bg2: "+B+"; --border: "+C+"; --text1: "+D+"; --text2: "+E;
 			block.innerHTML = `
 <img src="SRC">
@@ -52,7 +53,7 @@ function addRepos() {
 	<span>DESC</span>
 </div>
 `.replace("SRC", src).replace("TITLE", title).replace("DESC", desc);
-			sections[i].appendChild(block);
+			container.appendChild(block);
 		}
 	}
 }
