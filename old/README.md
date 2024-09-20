5141b36b08452cf4d71b205c8c13cb47b7706462
D_00 <leo.daloz@epita.fr>
2024-09-20 07:41:45 +0200

    Deleted old readme
    Using double quotation marks

--- old/README.md
+++
@@ -1,2 +0,0 @@
-# [d-002.github.io](https://d-002.github.io)
-D_00 (d-002) GitHub home page
--- script.js
+++ script.js
@@ -36,14 +36,14 @@ let repos = [
 // from SO/q/13382516
 function getScrollbarWidth() {
 	// Creating invisible container
-	const outer = document.createElement('div');
-	outer.style.visibility = 'hidden';
-	outer.style.overflow = 'scroll'; // forcing scrollbar to appear
-	outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+	const outer = document.createElement("div");
+	outer.style.visibility = "hidden";
+	outer.style.overflow = "scroll"; // forcing scrollbar to appear
+	outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
 	document.body.appendChild(outer);
 
 	// Creating inner element and placing it in the container
-	const inner = document.createElement('div');
+	const inner = document.createElement("div");
 	outer.appendChild(inner);
 
 	// Calculating difference between container's full width and the child width
