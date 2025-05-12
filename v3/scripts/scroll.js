e1a254bdfbba4ebe42cbec3f26e087a52526a8f0
D_00 <69427207+d-002@users.noreply.github.com>
2025-05-12 15:16:32 +0100

    Fixed scroll to top button not disappearing

--- v3/scripts/scroll.js
+++ v3/scripts/scroll.js
@@ -61,6 +61,7 @@ function scroll_handler() {
 
 const elts = Array.from(document.querySelectorAll(".animate-on-scroll")).map(elt => new ScrollElt(elt));
 const to_top = new ScrollElt(document.getElementById("to-top"));
+to_top.prevent_hiding = false;
 
 window.addEventListener("scroll", scroll_handler);
 scroll_handler(); // force update on load to make sure the already visible elements are animated
