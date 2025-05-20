60458ade2d0884d06f020816688fa012ac17fe41
D_00 <69427207+d-002@users.noreply.github.com>
2025-05-20 21:02:04 +0100

    Light up education tiles on scroll

--- v3/scripts/education.js
+++ v3/scripts/education.js
@@ -3,6 +3,8 @@ import is_active from "/v3/scripts/sleep.js";
 
 // in seconds
 const delta_time = get_dtime();
+// from 0 to 1
+const target_y = .38;
 
 class SmoothPoint {
     constructor(pos) {
@@ -19,18 +21,37 @@ class SmoothPoint {
 
 function get_percentage() {
     const rect = tiles_elt.getBoundingClientRect();
-    const scroll = .38 - rect.top / window.innerHeight;
+    const scroll = target_y - rect.top / window.innerHeight;
     const t = scroll * window.innerHeight / rect.height;
     return t < 0 ? 0 : t > 1 ? 100 : t*100; 
+}
+
+function is_tile_lower(tile) {
+    const rect = tile.getBoundingClientRect();
+    return rect.top + rect.height/2 > target_y * window.innerHeight;
 }
 
 function update() {
     if (!is_active(education_elt)) return;
 
+    // update point position
     const percent = get_percentage();
     point.update(percent);
 
     line.style = "--top: " + point.pos + "%";
+
+    // update tiles
+    const to_remove = [];
+    let i = -1;
+    inactive_tiles.forEach(tile => {
+        i++;
+        if (tile.classList.contains("active")) return;
+        if (is_tile_lower(tile)) return;
+
+        tile.classList.add("active");
+        to_remove.unshift(i);
+    });
+    to_remove.forEach(i => inactive_tiles.splice(i, 1));
 }
 
 const education_elt = document.getElementById("education");
@@ -38,5 +59,6 @@ const tiles_elt = education_elt.querySelector(".tiles-container");
 const line = education_elt.querySelector(".line");
 
 const point = new SmoothPoint(get_percentage());
+const inactive_tiles = Array.from(tiles_elt.children);
 
 window.setInterval(update, 1000 * delta_time);
